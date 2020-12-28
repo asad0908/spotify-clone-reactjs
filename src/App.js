65f2fb75b9ffe4af8a22c17c4b8539533c770a38
Asad Memon <amdeveloping0908@gmail.com>
2020-12-28 15:26:31 +0530

    created login feature using api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Login from "./component/Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return <div className="App"></div>;
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+  return (
+    <div className="app">{token ? <h1>I AM LOGGED IN</h1> : <Login />}</div>
+  );
 }
 
 export default App;
